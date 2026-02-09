5a435895b2205ff48d43fa236c309cb4a621e826
natly0909 <33386907+natly0909@users.noreply.github.com>
2026-02-09 21:53:58 +0100

    Update backup_google.sh

--- backup_google.sh
+++ backup_google.sh
@@ -6,14 +6,19 @@ rm -f /home/user/domains/backup/moodle*.sql.gz
 rm -f /home/user/domains/backup/moodle*.tar.gz
 
 tar -czf /home/user/domains/backup/moodle_${DATE}.tar.gz /home/user/domains/teaching.kse.org.ua/public_html
-mysqldump -uuser_teach -pCObTJo0vV user_kse | /usr/bin/gzip > /home/user/domains/backup/moodle_${DATE}.sql.gz
+time mysqldump -uuser_teach -pCObTJo0vV user_kse  --single-transaction --quick| /usr/bin/gzip > /home/user/domains/backup/moodle_${DATE}.sql.gz
 
 rclone delete gdrive:backups --min-age 3d --include "moodle*.tar.gz"
 rclone delete gdrive:backups --min-age 3d --include "moodle*.sql.gz"
 
-rclone copy /home/user/domains/private gdrive:backups/moodledata \
+time rclone sync /home/user/domains/private gdrive:backups/moodledata \
   --exclude "cache/**" \
   --exclude "localcache/**" \
   --exclude "temp/**" \
   --exclude "trashdir/**" \
   --exclude "lock/**" \
+  --transfers=2 \
+  --checkers=4 \
+  --retries 5 \
+  --log-file=/var/log/rclone-backup.log \
+  --log-level INFO
