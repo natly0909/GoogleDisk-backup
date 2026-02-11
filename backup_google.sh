f3ea96bb53a9a2c6e22016f89520ac39ff560670
natly0909 <33386907+natly0909@users.noreply.github.com>
2026-02-11 01:02:32 +0100

    Refactor backup_google.sh for improved file handling
    Меньшие архивы, правильно сделаны исключения, в tar добавлена команда -С

--- backup_google.sh
+++ backup_google.sh
@@ -1,26 +1,28 @@
-!/bin/bash
+#!/bin/bash
 
 DATE=$(date +%Y-%m-%d)
 
-rm -f /home/user/domains/backup/moodle_${DATE}.sql.gz
-rm -f /home/user/domains/backup/moodle_${DATE}.tar.gz
-rm -f /home/user/domains/backup/moodledata_${DATE}.tar.gz.part*
+rm -f /home/user/domains/backup/moodle_*.sql.gz
+rm -f /home/user/domains/backup/moodle_*.tar.gz
+rm -f /home/user/domains/backup/moodledata_*.tar.gz.part*
 
 time tar -czf /home/user/domains/backup/moodle_${DATE}.tar.gz \
-  /home/user/domains/teaching.kse.org.ua/public_html
-
+  -C /home/user/domains teaching.kse.org.ua/public_html
+  
 time mysqldump -uuser_teach -p'CObTJo0vV' user_kse \
   --single-transaction --quick \
 | /usr/bin/gzip > /home/user/domains/backup/moodle_${DATE}.sql.gz
 
-time bash -c "tar -cf - -C /home/user/domains/private . \
-  --exclude='cache/*' \
-  --exclude='localcache/*' \
-  --exclude='temp/*' \
-  --exclude='trashdir/*' \
-  --exclude='lock/*' \
+time bash -c "tar -cf - -C /home/user/domains private \
+  --exclude='private/cache/**' \
+  --exclude='private/localcache/**' \
+  --exclude='private/temp/**' \
+  --exclude='private/trashdir/**' \
+  --exclude='private/lock/**' \
 | gzip \
-| split -d -b 29000m - /home/user/domains/backup/moodledata_${DATE}.tar.gz.part"
+| split -d -b 8000m - /home/user/domains/backup/moodledata_${DATE}.tar.gz.part"
+
+
 
 rclone delete gdrive:backups --min-age 3d --include "moodle*.tar.gz"
 rclone delete gdrive:backups --min-age 3d --include "moodle*.sql.gz"
